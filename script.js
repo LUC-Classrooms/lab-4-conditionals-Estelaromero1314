fc62ed842e400212d1498cc8fb0398e5a0e160f1
Estelaromero1314 <157331722+Estelaromero1314@users.noreply.github.com>
2024-04-18 06:47:55 +0000

    lab 4 complete

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function setup() {
 }
 
 function draw() {
-  background(200);
+  background(220);
   
   // check to condition of the mouse
   if(mouseIsPressed){ // true or false?
     // if true, change the fill color here
-    
+    fill(255, 0, 0);
   }
   else { 
     // otherwise do this:
-    
+    fill(0, 0, 255)
   }
   ellipse(width/2, height/2, 100, 100);
 }
